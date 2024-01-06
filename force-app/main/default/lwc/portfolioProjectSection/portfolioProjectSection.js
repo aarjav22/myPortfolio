bb07ce1f2ea69269adba35271b49e30534cbca82
Aarjav Jain <aarjavoreo@gmail.com>
2024-01-06 17:10:00 +0530

    Project Section v1

--- force-app/main/default/lwc/portfolioProjectSection/portfolioProjectSection.js
+++ force-app/main/default/lwc/portfolioProjectSection/portfolioProjectSection.js
@@ -1,3 +1,9 @@
 import { LightningElement } from 'lwc';
+import PortfolioAssets from "@salesforce/resourceUrl/PortfolioAssets";
 
-export default class PortfolioProjectSection extends LightningElement {}
+export default class PortfolioProjectSection extends LightningElement {
+    githubIcon = PortfolioAssets + '/resource/link.png';
+
+    project1 = 'https://github.com/aarjav22/myPortfolio';
+    project2 = 'https://github.com/aarjav22/Handwritten_Digits_Recorgnizer';
+}
