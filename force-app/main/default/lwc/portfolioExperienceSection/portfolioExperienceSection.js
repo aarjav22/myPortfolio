7be22e9bf795b617545d8e2876364098e8b286ab
Aarjav Jain <aarjavoreo@gmail.com>
2025-03-17 20:03:50 +0530

    Added experience

--- force-app/main/default/lwc/portfolioExperienceSection/portfolioExperienceSection.js
+++ force-app/main/default/lwc/portfolioExperienceSection/portfolioExperienceSection.js
@@ -3,4 +3,5 @@ import PortfolioAssets from "@salesforce/resourceUrl/PortfolioAssets";
 
 export default class PortfolioExperienceSection extends LightningElement {
     company1 = PortfolioAssets + '/resource/Persistent.png';
+    company2 = PortfolioAssets + '/resource/Accenture.png';
 }
